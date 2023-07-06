db1c6979622a3c67d556b9e66445f27476e704fa
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-07-06 02:10:05 -0400

    Erase a comment

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,3 @@
-# Implement a method called correct_name that will raise a NotImplementedError.
-
 class Nameable
   def correct_name
     raise NotImplementedError, 'Implement this method in a child class'
