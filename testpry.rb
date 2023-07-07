62c77a2bcb47bfa6a32f1fdab9d0f2ae87bb0fc3
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-07-07 18:24:02 -0400

    making tests

---
+++ testpry.rb
@@ -0,0 +1,16 @@
+# testpry.rb
+require 'pry'
+
+class A
+  def hello()
+    puts 'hello world!'
+  end
+end
+
+a = A.new
+
+# start a REPL session
+binding.pry
+
+# program resumes here (after pry session)
+puts 'program resumes here.'
