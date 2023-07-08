72dbb1f8fa7a739daef144820e1d3eb22c86419d
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-07-08 04:26:38 -0400

    Updated student.rb

--- student.rb
+++ student.rb
@@ -3,9 +3,12 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
-    @classroom = classroom
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission:)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+    @classroom = nil
   end
 
   def classroom=(classroom)
